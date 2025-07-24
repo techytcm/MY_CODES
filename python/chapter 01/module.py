07c6bfa3077384ff9ab94418a58476e0ec5ab191
Tonmoy Chandra Mudi <tcm9798@gmail.com>
2025-07-25 01:15:36 +0600

    Delete python directory

--- python/chapter 01/module.py
+++
@@ -1,7 +0,0 @@
-import pyjokes
-print("Welcome to the Joke Generator!")
-#This prints a random jokes
-# single line comment shortcut ctrl /
-print("Here's a joke for you:")
-joke = pyjokes.get_joke()
-print(joke)
