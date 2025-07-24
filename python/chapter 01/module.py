43485778aad3b056a4bb72abf1f037d578ddda93
Tonmoy Chandra Mudi <mudi242-35-507@diu.edu.bd>
2025-07-25 01:06:52 +0600

    comment

--- python/chapter 01/module.py
+++ python/chapter 01/module.py
@@ -1,5 +1,6 @@
 import pyjokes
-#print("Welcome to the Joke Generator!")
+print("Welcome to the Joke Generator!")
+#This prints a random jokes
 print("Here's a joke for you:")
 joke = pyjokes.get_joke()
 print(joke)
