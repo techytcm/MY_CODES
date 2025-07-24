cf4cc8b78c707bf1df18b24ac05ffc8bd054f9c7
Tonmoy Chandra Mudi <mudi242-35-507@diu.edu.bd>
2025-07-25 01:09:05 +0600

    single line comment shortcut

--- python/chapter 01/tempCodeRunnerFile.py
+++ python/chapter 01/tempCodeRunnerFile.py
@@ -1,7 +1,6 @@
 import pyjokes
 print("Welcome to the Joke Generator!")
 #This prints a random jokes
-# single line comment shortcut ctrl /
 print("Here's a joke for you:")
 joke = pyjokes.get_joke()
 print(joke)
